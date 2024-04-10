e637bbf5fab30de3857435395544977183587bda
kylian AUBRY <aubry.kylian973@gmail.com>
2024-04-10 21:56:15 +0200

    start storage elector

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -3,5 +3,33 @@
 pragma solidity 0.8.25;
 
 contract Ballot {
-    
+    struct Cendidate {
+        string name;
+        uint voteCount;
+    }
+
+    struct Elector{
+        address delegate;
+        uint electoralNumber;
+        bool voted;
+        uint vote;
+    }
+
+    mapping(uint => address) private adresses;
+    mapping (address => Elector) public electors;
+    Cendidate[] public cendidates;
+
+    function register(uint _number) public
+    {
+        require(adresses[_number] == address(0), "this number already exists.");
+        require(electors[msg.sender].electoralNumber == 0, "this address is already assigned to a number.");
+
+        //ici implementer une verification sur le numero savoir si le numero electorale est valide ou non.
+
+        Elector memory newPerson = Elector(msg.sender, _number, false, 0);
+        electors[msg.sender] = newPerson;
+        adresses[_number] = msg.sender;
+    }
+
+    // function vote(uint _number, )
 }
